764b3ba88f305ebb2febd9f37a81d3dcf190f5b9
elsh1923 <elsh1923@users.noreply.github.com>
2026-03-18 00:19:57 +0300

    Add breathing space between navbar and all pages; fix sidebar overlap with navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
           >
             <div className="flex min-h-screen flex-col">
               <Navbar />
-              <main className="flex-1">{children}</main>
+              <main className="flex-1 mt-8">{children}</main>
               <FooterWrapper />
             </div>
           </ThemeProvider>
